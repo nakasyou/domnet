69716d9140ec264f326ae6c8d80a86cdd4292111
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-11-13 08:23:20 +0000

    feat: add more domains

--- format-tlds.ts
+++ format-tlds.ts
@@ -1,6 +1,6 @@
-import tlds from './tlds.json' assert { type: 'json' }
+import tlds from './tlds.json' with { type: 'json' }
 
 tlds.tlds = [...new Set(tlds.tlds)]
-tlds.tlds.sort()
+tlds.tlds.sort((a, b) => a.split('.').reverse().join('.') < b.split('.').reverse().join('') ? -1 : 1)
 
 await Deno.writeTextFile('tlds.json', JSON.stringify(tlds, null, 2))
